9b3f734bb4edd245a4c244643c22ffe99c0f0704
Amira K <129123642@aston.ac.uk>
2020-10-21 21:36:41 +0100

    fixed typo in variable name

--- src/model/Model.java
+++ src/model/Model.java
@@ -2,5 +2,5 @@ package model;
 
 public class Model {
 
-    public int impottanceIndex[] = {0 , 1, 2, 3, 4, 5, 6, 7 , 8, 9, 10};
+    public int importanceIndex[] = {0 , 1, 2, 3, 4, 5, 6, 7 , 8, 9, 10};
 }
