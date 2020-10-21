061d852fbf61eaef1ee068b722dc542edd609e14
Amira K <129123642@aston.ac.uk>
2020-10-21 21:04:47 +0100

    Amira first commit to test gitlab

--- src/model/Model.java
+++ src/model/Model.java
@@ -2,6 +2,7 @@ package model;
 
 public class Model {
 
-    public Integer improtanceIndex[]
-            = {0 , 1, 2, 3, 4, 5, 6, 7 , 8, 9, 10};
+    public int impottanceIndex[] = {0 , 1, 2, 3, 4, 5, 6, 7 , 8, 9, 10};
+			
+	public int urgencyValue;
 }
