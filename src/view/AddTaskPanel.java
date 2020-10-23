54de319ebe2a7a1aa663f0e7ae006fc797175bc9
Alamgir Mohammed <200258908@aston.ac.uk>
2020-10-23 01:27:04 +0100

    Required Boolean Added

--- src/view/AddTaskPanel.java
+++ src/view/AddTaskPanel.java
@@ -1,7 +1,13 @@
 package view;
 import model.Model;
+import model.Task;
+import model.TaskRepository;
+
 import javax.swing.*;
-import java.awt.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+import java.awt.Color;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -47,43 +53,69 @@ public class AddTaskPanel extends AbstractCommonComponents {
     private Model model;
     private JButton addTaskButton;
 
-    public AddTaskPanel(String addTaskPanelTitle, Model model,int x, int y, int width, int height, Color color, Boolean createBorder, int boundsHeight) {
+    public AddTaskPanel(String addTaskPanelTitle, Model model, int x, int y, int width, int height, Color color,
+                        Boolean createBorder, int boundsHeight, TaskTreePanel taskTreePanel, TaskRepository repository) {
         super(addTaskPanelTitle, model, x, y, width,height, color, createBorder, boundsHeight);
         this.model = model;
 
+        DefaultMutableTreeNode node = new DefaultMutableTreeNode("banana");
+        node.add(new DefaultMutableTreeNode("yellow"));
+        taskTreePanel.addTreeNode(node);
+        taskTreePanel.addTreeNode(new DefaultMutableTreeNode("guava"));
+
+        List<AddTaskFormField> formFields = Arrays.asList(
+                new AddTaskFormField("Project Name:", true, color),
+                new AddTaskFormField("Description", true, color),
+                new AddTaskFormField("Sub Task 1:", true, color),
+                new AddTaskFormField("Sub Task 2:", false, color),
+                new AddTaskFormField("Sub Task 3:", false, color),
+                new AddTaskFormField("Sub Task 4:", false, color),
+                new AddTaskFormField("Sub Task 5:", false, color)
+                //new AddTaskFormField("Due Date:", true, color),
+                //new AddTaskFormField("Due Time:", true, color)
+
+
+        );
+
         //Project Name and field
-        createJLabel(projectName, "Project Name:",5 , 20, FIELD_START - 2, 15, color);
-        createJTextField(projectNameEntry, FIELD_START , 18, 3*(this.getWidth()-120)/4, 20);
+        //createJLabel(projectName, "Project Name:",5 , 20, FIELD_START - 2, 15, color);
+        //createJTextField(projectNameEntry, FIELD_START , 18, 3*(this.getWidth()-120)/4, 20);
+
+        int fieldY = 20;
+        for (AddTaskFormField formField : formFields) {
+            fieldY = formField.addControls(this, fieldY);
+            fieldY += 5;
+        }
 
 
         //Description and field name.
-        createJLabel(description, "Description:",5 , 45, FIELD_START - 2, 15, color);
-        createJTextField(descriptionEntry, FIELD_START , 43, this.getWidth()-120, 20);
+        //createJLabel(description, "Description:",5 , 45, FIELD_START - 2, 15, color);
+        //createJTextField(descriptionEntry, FIELD_START , 43, this.getWidth()-120, 20);
       
         //Task Name Label and field name
-        createJLabel(taskName, "Task Name",5 , 70, FIELD_START - 2, 15, color);
-        createJTextField(taskNameEntry, FIELD_START , 68, 3*(this.getWidth()-120)/4, 20);
+        //createJLabel(taskName, "Task Name",5 , 70, FIELD_START - 2, 15, color);
+        //createJTextField(taskNameEntry, FIELD_START , 68, 3*(this.getWidth()-120)/4, 20);
 
        
         //sub-task 1 (Needs to be made mandatory)
-        createJLabel(subTask1, "Sub Task 1:",5, 95, FIELD_START - 2, 15, color);
-        createJTextField(subTask1NameEntry, FIELD_START , 93, 3*(this.getWidth()-120)/4, 20);
+        //createJLabel(subTask1, "Sub Task 1:",5, 95, FIELD_START - 2, 15, color);
+        //createJTextField(subTask1NameEntry, FIELD_START , 93, 3*(this.getWidth()-120)/4, 20);
 
         //sub-task 2 (optional)
-        createJLabel(subTask2, "Sub Task 2:",5 , 120, FIELD_START - 2, 15, color);
-        createJTextField(subTask2NameEntry, FIELD_START , 118, 3*(this.getWidth()-120)/4, 20);
+        //createJLabel(subTask2, "Sub Task 2:",5 , 120, FIELD_START - 2, 15, color);
+        //createJTextField(subTask2NameEntry, FIELD_START , 118, 3*(this.getWidth()-120)/4, 20);
 
         //sub-task 3 (optional)
-        createJLabel(subTask3, "Sub Task 3:",5 , 145, FIELD_START - 2, 15, color);
-        createJTextField(subTask3NameEntry, FIELD_START , 143, 3*(this.getWidth()-120)/4, 20);
+        //createJLabel(subTask3, "Sub Task 3:",5 , 145, FIELD_START - 2, 15, color);
+        //createJTextField(subTask3NameEntry, FIELD_START , 143, 3*(this.getWidth()-120)/4, 20);
 
         //sub-task 4 (optional)
-        createJLabel(subTask4, "Sub Task 4:",5 , 170, FIELD_START - 2, 15, color);
-        createJTextField(subTask4NameEntry, FIELD_START , 168, 3*(this.getWidth()-120)/4, 20);
+        //createJLabel(subTask4, "Sub Task 4:",5 , 170, FIELD_START - 2, 15, color);
+        //createJTextField(subTask4NameEntry, FIELD_START , 168, 3*(this.getWidth()-120)/4, 20);
 
         //sub-task 5 (optional)
-        createJLabel(subTask5, "Sub Task 5:",5 , 195, FIELD_START - 2, 15, color);
-        createJTextField(subTask5NameEntry, FIELD_START , 193, 3*(this.getWidth()-120)/4, 20);
+        //createJLabel(subTask5, "Sub Task 5:",5 , 195, FIELD_START - 2, 15, color);
+        //createJTextField(subTask5NameEntry, FIELD_START , 193, 3*(this.getWidth()-120)/4, 20);
 
 
         //Task due date field
@@ -114,10 +146,14 @@ public class AddTaskPanel extends AbstractCommonComponents {
         createJTextField(estimatedTaskDurationField, FIELD_START, 281, (this.getWidth()-220)/2, 20);
 
         //create buttons
-        createJButton(addTaskButton,"Add Task", this.getWidth()/4-50, height-42, 100, 40, true,Color.blue,true);
-        createJButton(taskUpdateButton,"Update Task",this.getWidth()/2-50, height-42, 100,40, false, Color.blue, true);
-        createJButton(clearTaskButton,"Clear Field",3*this.getWidth()/4-50, height-42, 100,40, false, Color.blue, true);
-        
+        JButton addTaskButton = createJButton(null,"Add Task", this.getWidth()/4-50, height-42, 100, 40, true,Color.blue,true);
+        createJButton(null,"Update Task",this.getWidth()/2-50, height-42, 100,40, false, Color.blue, true);
+        createJButton(null,"Clear Field",3*this.getWidth()/4-50, height-42, 100,40, false, Color.blue, true);
+
+        addTaskButton.addActionListener((e)-> {
+            Task task = new Task();
+            repository.add(task);
+        });
 
     }
 }
